80ca82c04a25dde54f5ad4da5c68c0e2abab9094
Gerard-Menfin <nordinateur@schie.ml>
2024-10-21 17:02:22 +0200

    Fix : ⛔ redirection 1ère connexion

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ if(!empty($_SESSION["auth"])  && $_SERVER["HTTP_USER_AGENT"] == $_SESSION["HTTP_
                 $_SESSION["HTTP_USER_AGENT"] = $_SERVER["HTTP_USER_AGENT"];
                 $_SESSION["session"]         = $_COOKIE[session_name()];
                 $_SESSION["ip"]              = $_SERVER["SERVER_ADDR"];
-                redirect("/$user"); 
+                redirect("/$userName"); 
             }
             break;
     }
