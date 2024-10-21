fca535eee13f23c0a31f1ed79a6e4d8febba31f2
Gerard-Menfin <nordinateur@schie.ml>
2024-10-21 14:55:09 +0200

    FIX :  event JS         ⛔ dispatchEvent(new Event("submit"))          ✅ submit()

--- assets/js/ldc.js
+++ assets/js/ldc.js
@@ -1,10 +1,11 @@
+console.log("%Chargement du JS");
+
 const frm = document.querySelector("form#list");
-// const cbs = document.querySelectorAll("input[type='checkbox'].crossout");
 const cbs = document.querySelectorAll("input[type='checkbox']");
 
 for (const cb of cbs) {
     cb.addEventListener("change", e => {
-        vf = frm.dispatchEvent(new Event("submit"));
+        frm.submit();
     });
 
 }
