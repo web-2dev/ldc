fbe2063733d7317e001f3273e2ab277e820fa83a
Gerard-Menfin <nordinateur@schie.ml>
2024-10-21 14:14:31 +0200

    FIX : > ⛔ chargement du JS > ✅ URL relative

--- views/header.html.php
+++ views/header.html.php
@@ -4,16 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste De Courses</title>
+
     <!-- ICONE -->
     <link rel="shortcut icon" href="assets/images/de_32x32.ico" type="image/x-icon">
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-    <!-- <link rel="stylesheet" href="../assets/css/bootstrap-icons.min.css"> -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">    
     
-    <link rel="stylesheet" href="../assets/css/ldc.css">
+    <link rel="stylesheet" href="assets/css/ldc.css">
     
-    <script src="../assets/js/bootstrap.bundle.min.js"></script>
-    <script src="../assets/js/ldc.js" defer></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/ldc.js" defer></script>
     
 </head>
 <body>
