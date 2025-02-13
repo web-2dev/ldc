f5c4d731a6c2ad33e7c178182ea9178fa015ff94
web-2dev <didier@web2dev.fr>
2025-02-13 22:35:58 +0100

    fix JS : submit doesn't launch on mobile app

--- assets/js/ldc.js
+++ assets/js/ldc.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const frm = document.querySelector("form#list");
-    // const cbs = document.querySelectorAll("input[type='checkbox'].crossout");
     const cbs = document.querySelectorAll("input[type='checkbox']");
     for (const cb of cbs) {
         cb.addEventListener("change", e => {
-            vf = frm.dispatchEvent(new Event("submit"));
+           frm.submit();
         });
     }
-
     document.querySelector("input[name='add']").focus();
 });
